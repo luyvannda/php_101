4fda96c5f09add29bc74b8fd320f8cabeecc4ef6
luyvannda <philips.joe329@gmail.com>
2024-01-09 16:01:26 +0700

    complete php homework to display year of book released next to book's title

--- index.php
+++ index.php
@@ -3,10 +3,8 @@
 
   <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Demo</title>
-
   </head>
 
   <body>
@@ -15,43 +13,39 @@
     $books = [
       [
         "title" => "Do Androids Dream of Electric Sheep",
+        "releasedYear" => 1998,
         "author" => "Joe Joe",
         "purchaseUrl" => "www.example1.com",
       ],
 
       [
         "title" => "The Langoliers",
+        "releasedYear" => 2008,
         "author" => "Mary Jane",
         "purchaseUrl" => "www.example2.com",
       ],
 
       [
         "title" => "Hail Mary",
+        "releasedYear" => 2020,
         "author" => "Peter Parker",
         "purchaseUrl" => "www.example3.com",
       ]
     ]
 
       ?>
-
     <h1>Recommended Books</h1>
-
     <ul>
-
       <?php foreach ($books as $book): ?>
-
         <li>
           <a href="<?= $book["purchaseUrl"] ?>">
             <?= "{$book["title"]}&trade;" ?>
           </a>
-          by
-          <?= $book["author"] ?>
+          (<?= $book["releasedYear"] ?>)
+          by <?= $book["author"] ?>.
         </li>
-
       <?php endforeach; ?>
-
     </ul>
-
   </body>
 
 </html>
